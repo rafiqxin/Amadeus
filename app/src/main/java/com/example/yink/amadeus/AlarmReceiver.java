39f6103a8735bb304b909a4ccfc1c404ef043cdd
rafiqxin <xinmochaa@gmail.com>
2026-09-10 00:53:30 -0700

    Run alarm playback through a foreground service

--- app/src/main/java/com/example/yink/amadeus/AlarmReceiver.java
+++ app/src/main/java/com/example/yink/amadeus/AlarmReceiver.java
@@ -5,35 +5,52 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
-    private final String TAG = "AlarmReceiver";
+    private static final String TAG = "AlarmReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        Log.d(TAG, "Broadcast received!");
+        Log.d(TAG, "Broadcast received");
 
         int[] ringtones = {
-                R.raw.ringtone_gate_of_steiner, R.raw.ringtone_village,
-                R.raw.ringtone_beginning_of_fight, R.raw.ringtone_easygoingness,
-                R.raw.ringtone_reunion, R.raw.ringtone_precaution,
+                R.raw.ringtone_gate_of_steiner,
+                R.raw.ringtone_village,
+                R.raw.ringtone_beginning_of_fight,
+                R.raw.ringtone_easygoingness,
+                R.raw.ringtone_reunion,
+                R.raw.ringtone_precaution,
                 R.raw.ringtone_over_the_sky
         };
 
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
-        int index = Integer.parseInt(settings.getString("ringtone", "0"));
-
-        Log.d(TAG, "Starting alarm...");
-
-        Alarm.start(context, ringtones[index]);
+        int index;
+        try {
+            index = Integer.parseInt(settings.getString("ringtone", "0"));
+        } catch (NumberFormatException e) {
+            index = 0;
+        }
+        if (index < 0 || index >= ringtones.length) {
+            index = 0;
+        }
 
         Intent service = new Intent(context, AlarmService.class);
-        context.startService(service);
+        service.putExtra(AlarmService.EXTRA_RINGTONE, ringtones[index]);
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(service);
+            } else {
+                context.startService(service);
+            }
+        } catch (RuntimeException e) {
+            Log.e(TAG, "Unable to start alarm service", e);
+        }
+
         setResultCode(Activity.RESULT_OK);
     }
-
 }
